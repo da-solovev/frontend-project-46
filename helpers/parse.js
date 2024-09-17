a6441ecba1e319c20055a9ad4dcf291ad0275fd5
da-solovev <solo.dmit1994@gmail.com>
2024-09-17 23:10:46 +0300

    Edit parse

--- helpers/parse.js
+++ helpers/parse.js
@@ -1,8 +1,3 @@
-import { readFileSync } from 'fs';
-
-export const parseToJSON = (path) => {
-  const file = readFileSync(path, 'utf8');
-  const fileToJSON = JSON.parse(file);
-  
-  return fileToJSON;
+export const parse = (str) => {
+  return JSON.parse(str);
 }
