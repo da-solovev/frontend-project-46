c0aecc8983d3a4eed55ff995a85919ea84ef4c6a
da-solovev <solo.dmit1994@gmail.com>
2024-10-18 01:10:15 +0300

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import getParseData from './parsers.js';
-import calcDiff from './calcDiff.js';
 import buildDiff from '../formatters/index.js';
+import calcDiff from './calcDiff.js';
 
 const genDiff = (filepath1, filepath2, format) => {
   const object1 = getParseData(filepath1);
   const object2 = getParseData(filepath2);
-  const diff = (calcDiff(object1, object2));
+  const diff = calcDiff(object1, object2);
   return buildDiff(diff, format);
 };
 export default genDiff;
