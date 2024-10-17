17e69f5151fe31126b9b0c332b2f64f471e404ad
da-solovev <solo.dmit1994@gmail.com>
2024-10-17 22:45:16 +0300

    Refactoring bin

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,9 @@
+#!/usr/bin/env node
+
 import { Command } from 'commander';
-import { parse } from '../helpers/parsers.js';
+import getParseData from '../src/parsers.js';
 import genDiff from '../src/diff.js';
+import buildDiff from '../formatters/index.js';
 
 const program = new Command();
 
@@ -10,11 +13,13 @@ program
   .version('0.1.0')
   .argument('filepath1', 'path input file1')
   .argument('filepath2', 'path input file2')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format: stylish (default), plain, json')
   .action((filepath1, filepath2) => {
-    const object1 = parse(filepath1);
-    const object2 = parse(filepath2);
-    console.log(genDiff(object1, object2));
+    const options = program.opts();
+    const object1 = getParseData(filepath1);
+    const object2 = getParseData(filepath2);
+    const diff = (genDiff(object1, object2));
+    console.log(buildDiff(diff, options.format));
   });
 
 program.parse(process.argv);
--- src/parsers.js
+++ src/parsers.js
@@ -1,17 +1,25 @@
 import yaml from 'js-yaml';
 import path from 'path';
+import fs from 'fs';
 
-export const parseJSON = (str) => JSON.parse(str);
+const parseJSON = (str) => JSON.parse(str);
 
-export const parseYaml = (str) => yaml.load(str);
+const parseYaml = (str) => yaml.load(str);
 
-export const parse = (filepath) => {
+const parse = (filepath, data) => {
   const fileExt = path.extname(filepath);
   if (fileExt === '.json') {
-    return parseJSON(filepath);
+    return parseJSON(data);
   }
   if (['.yaml', '.yml'].includes(fileExt)) {
-    return parseYaml(filepath);
+    return parseYaml(data);
   }
   throw new Error(`File extension ${fileExt} not supported`);
 };
+
+const getParseData = (filePath) => {
+  const data = fs.readFileSync(filePath, 'utf-8');
+  const parseData = parse(filePath, data);
+  return parseData;
+};
+export default getParseData;
