c00f217e0456e651ddab9218301d479efcb9590b
da-solovev <solo.dmit1994@gmail.com>
2024-11-11 23:51:02 +0300

    Fix plain lines break

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -14,22 +14,25 @@ const iter = (item, property = []) => {
   const keys = [...property, item.key];
   const pathProperty = keys.join('.');
   if (item.type === 'added') {
-    return `Property '${pathProperty}' was added with value: ${formatValue(item.value)}\n`;
+    return `Property '${pathProperty}' was added with value: ${formatValue(item.value)}`;
   }
   if (item.type === 'deleted') {
-    return `Property '${pathProperty}' was removed\n`;
+    return `Property '${pathProperty}' was removed`;
   }
   if (item.type === 'changed') {
-    return `Property '${pathProperty}' was updated. From ${formatValue(item.value1)} to ${formatValue(item.value2)}\n`;
+    return `Property '${pathProperty}' was updated. From ${formatValue(item.value1)} to ${formatValue(item.value2)}`;
   }
   if (item.type === 'unchanged') {
     return '';
   }
-  return item.children.map((key) => iter(key, [pathProperty])).join('');
+  return item.children
+    .map((key) => iter(key, [pathProperty]))
+    .filter(Boolean)
+    .join('\n');
 };
 
 const formatPlain = (diff) => {
-  const result = diff.map((key) => iter(key)).join('').trim();
+  const result = diff.map((key) => iter(key)).join('\n').trim();
   return result;
 };
 export default formatPlain;
