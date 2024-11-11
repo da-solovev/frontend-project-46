9bc324862869da1ab030e73d6caf8322709b8e78
da-solovev <solo.dmit1994@gmail.com>
2024-11-11 23:08:43 +0300

    Fix stylish line break

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -16,25 +16,25 @@ const stringify = (item, depth) => {
 const iter = (item, depth = 1) => {
   const indent = calcIndent(depth).slice(0, -2);
   if (item.type === 'added') {
-    return `${indent}+ ${item.key}: ${stringify(item.value, depth)}\n`;
+    return `${indent}+ ${item.key}: ${stringify(item.value, depth)}`;
   }
   if (item.type === 'deleted') {
-    return `${indent}- ${item.key}: ${stringify(item.value, depth)}\n`;
+    return `${indent}- ${item.key}: ${stringify(item.value, depth)}`;
   }
   if (item.type === 'changed') {
-    return `${indent}- ${item.key}: ${stringify(item.value1, depth)}\n${indent}+ ${item.key}: ${stringify(item.value2, depth)}\n`;
+    return `${indent}- ${item.key}: ${stringify(item.value1, depth)}\n${indent}+ ${item.key}: ${stringify(item.value2, depth)}`;
   }
   if (item.type === 'unchanged') {
-    return `${indent}  ${item.key}: ${stringify(item.value, depth)}\n`;
+    return `${indent}  ${item.key}: ${stringify(item.value, depth)}`;
   }
-  const begin = `${indent}  ${item.key}: {\n`;
-  const node = item.children.map((key) => iter(key, depth + 1)).join('');
-  const end = `${indent}  }\n`;
+  const begin = `${indent}  ${item.key}: {`;
+  const node = `\n${item.children.map((key) => iter(key, depth + 1)).join('\n')}\n`;
+  const end = `${indent}  }`;
   return begin + node + end;
 };
 
 const fortamStylish = (diff) => {
-  const result = `{\n${diff.map((key) => iter(key)).join('')}}`;
+  const result = `{\n${diff.map((key) => iter(key)).join('\n')}\n}`;
   return result;
 };
 
